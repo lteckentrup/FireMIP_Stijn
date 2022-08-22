d7a629ecdea302681983533441acc10a90f59ede
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-08-22 16:46:21 +1000

    Create coarse_grid.sh

---
+++ coarse_grid.sh
@@ -0,0 +1,5 @@
+### Model names
+declare -a model_list=('CLASS-CTEM' 'CLM' 'Inferno' 'LPJ-GUESS-GlobFIRM'
+                       'LPJ-GUESS-SIMFIRE-BLAZE' 'LPJ-GUESS-SPITFIRE' 
+                       'ORCHIDEE-SPITFIRE')
+                       
