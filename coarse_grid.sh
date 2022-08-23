e6e10dbf84b81147dc22c30334b04318fd702784
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-08-23 11:47:30 +1000

    Delete coarse_grid.sh

--- coarse_grid.sh
+++
@@ -1,5 +0,0 @@
-### Model names
-declare -a model_list=('CLASS-CTEM' 'CLM' 'Inferno' 'LPJ-GUESS-GlobFIRM'
-                       'LPJ-GUESS-SIMFIRE-BLAZE' 'LPJ-GUESS-SPITFIRE' 
-                       'ORCHIDEE-SPITFIRE')
-                       
